c0f62c58eabde61bffaf2cd5a43f1d5d5d52aa9b
arbaz93 <arbaz.k480@yahoo.com>
2024-03-17 14:29:47 +0500

    Same thing as the last commit

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
  
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
-app.get('/home', (req, res) => {
-      app.use(express.static(__dirname));
+        app.use(express.static(__dirname));
         app.use(express.static('public'));
         app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname + '/public/index.html'));
-});
   })
  
 // Start the server
