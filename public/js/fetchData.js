0ea5823c6808f1391ecf9fc23c953af154c8fefb
arbaz93 <arbaz.k480@yahoo.com>
2024-03-17 19:51:46 +0500

    many things

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,10 +1,13 @@
+
 const productSection = document.querySelector(".products");
 const links = document.querySelectorAll(".shop-link");
 const sortBtns = document.querySelectorAll(".sort-btns > button");
-let productsData;
+let productsData = [];
+let pHeading = [];
 async function getData() {
-    const request = await fetch("/data/product-data.json"); //getting data
+    const request = await fetch("/data/data.json"); //getting data
     const data = await request.json(); // converting requested data into JSON
+
     return data;
 };
 
@@ -25,7 +28,7 @@ const appendData = () => {
                             <div class="flex product-data">
                                 <div class="flex">
                                     <a href="#">
-                                        <h4 id="product-heading">${product.name}</h4>
+                                        <h4 id="product-heading" data-id=${product.id}>${product.name}</h4>
                                     </a>
                                     <p id="product-type">${product.type}</p>
                                 </div>
@@ -38,6 +41,8 @@ const appendData = () => {
             html = html + template;
             })
             productSection.innerHTML = html;
+            pHeading = document.querySelectorAll("#product-heading");
+            addingEventListenerToProductHeadings();
     });
 }
 const sortData = (type) => {
@@ -76,7 +81,50 @@ const sortData = (type) => {
     productSection.innerHTML = html;
 
 }
-
 links.forEach(link => {
     link.addEventListener("click", appendData);
 });
+
+document.querySelector("[data-option='All']").addEventListener('click', () => {
+    appendData();
+})
+document.querySelector("[data-option='Bag']").addEventListener('click', () => {
+    sortData("Bag");
+})
+document.querySelector("[data-option='Jewelery']").addEventListener('click', () => {
+    sortData("Jewelery");
+})
+document.querySelector("[data-option='Accessory']").addEventListener('click', () => {
+    sortData("Accessory");
+})
+
+const putDataInSidePane = target => {
+    const targetId = target.dataset.id;
+    productsData.map(data => {
+        if (data.id == targetId) {
+            const image = document.querySelector(".selected-item-image > img").src = data.image;
+            const heading = document.querySelector(".selected-item-heading > h4").innerHTML = data.name;
+            const description = document.querySelector(".selected-item-description > p").innerHTML = data.description;
+            console.log(data)
+
+        }
+    })
+}
+let addingEventListenerToProductHeadings = () => {
+    pHeading.forEach(heading => {
+        heading.addEventListener("click", () => {
+            putDataInSidePane(heading);
+        })
+    })
+}
+let qty = document.querySelector("#qty");
+document.querySelector(".minus").addEventListener('click', () => {
+    if(qty.value > 1) {
+        qty.value = Number(qty.value) - 1;
+    }
+})
+document.querySelector(".plus").addEventListener('click', () => {
+    if(qty.value < 99) {
+        qty.value = Number(qty.value) + 1;
+    }
+})
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const { readFile, writeFile } = require('fs');
+let count = 0;
+// readFile('./public/data/product-data.json', 'utf-8', (err, res) => {}
+// readFile('./public/data/product-data.json', 'utf-8', (err, res) => {
+//     let data = JSON.parse(res);
+//     data.map(d => {
+//       d.id = count;
+//       count++;
+//     })
+//     let newData = JSON.stringify(data);
+//     writeFile('./public/data/data.json', newData, (err, res) => {
+//       console.log(res)
+//     })
+// });
 
 app.use(express.static(__dirname));
 app.use(express.static('public'));
