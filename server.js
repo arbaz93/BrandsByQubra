8ce333e3fe6e8c37c48da21d64745b72ccfc9e3e
arbaz93 <arbaz.k480@yahoo.com>
2024-03-17 14:33:03 +0500

    adding admin route to server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
- 
-        app.use(express.static(__dirname));
-        app.use(express.static('public'));
-        app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname + '/public/index.html'));
-  })
+
+app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.get('*', function(req, res) {
+res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/admin.html'));
+})
+
  
 // Start the server
 const PORT = process.env.PORT || 8080;
