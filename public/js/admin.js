f897433faedc44f11ee3af3d60e59d84fc67cdf7
arbaz93 <arbaz.k480@yahoo.com>
2024-03-19 22:44:38 +0500

    "Working on admin panel"

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,6 @@
+const form = document.querySelector(".add-item-form");
 
-
-async function retrieveJson() {
-    const request = await fetch('./data/product-data.json')
-    const data = await request.json();
-
-    console.log(data)
-    return data
-}
-
-retrieveJson();
+form.addEventListener("submit", function(event) {
+    console.log(event);
+    event.preventDefault();
+});
--- public/js/nav.js
+++ public/js/nav.js
@@ -1,3 +1,7 @@
 const navCollapse = document.querySelector('nav');
 const navOpen = () => navCollapse.dataset.navCollapsed = "true"
 const navClose = () => navCollapse.dataset.navCollapsed = "false"
+
+document.querySelector(".logo-section > .logo").addEventListener("dblclick", () => {
+    window.location.href = "/admin"
+})
