1223be619b264a3c8a4856f1e5bbaab0f84c8a30
arbaz93 <arbaz.k480@yahoo.com>
2024-03-20 17:26:57 +0500

    ""

---
+++ admin.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".add-item-form");
+import a from "/i.js"
+console.log(i)
+
+form.addEventListener("submit", function(event) {
+    console.log(event);
+    event.preventDefault();
+});
+
